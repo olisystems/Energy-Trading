b3a42e343b8286096c3dbab980e518d166fac1a2
Muhammad Faizan <33482427+faizan2590@users.noreply.github.com>
2017-11-08 22:24:46 +0100

    Update OliCoin.sol

--- Smart_Contract/OliCoin.sol
+++ Smart_Contract/OliCoin.sol
@@ -1,1 +1,41 @@
-#OliCoin
+pragma solidity ^0.4.11;
+
+import "github.com/ethereum/solidity/std/mortal.sol";
+
+contract OliCoin is mortal{
+    //Energy Balance Mapping
+    mapping (address => int) OliCoinBalance; //Produce->inc & Consume->dec
+    //Update CoinBalance
+    function set_OliCoinBalance(address _account, int _change) {
+        OliCoinBalance[_account] += _change;
+    }
+    
+    /// Withdraw earned OliCoins.
+    function withdraw() returns (bool) {
+        int amount = OliCoinBalance[msg.sender];
+        if (amount > 0) {
+            // It is important to set this to zero because the recipient
+            // can call this function again as part of the receiving call
+            // before `send` returns.
+            OliCoinBalance[msg.sender] = 0;
+            if (!msg.sender.send(uint(amount))) {
+                // No need to call throw here, just reset the amount owing
+                OliCoinBalance[msg.sender] = amount;
+                return false;
+            }
+        }
+        else {
+            return false;
+        }
+        return true;
+    }
+    
+    //Deposit OliCoins
+    function deposit(uint _value) onlyowner {
+        OliCoinBalance[msg.sender] += int(_value);
+    }
+    //
+    function get_coinBalance(address caddress) constant returns (int) {
+        return OliCoinBalance[caddress];
+    }
+}
