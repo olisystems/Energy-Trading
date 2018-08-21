dc42faedf74533a0ca4adb241387a491833cd3b1
Gamesplaining <felix.foerster@my-oli.com>
2018-08-21 13:06:27 +0200

    new truffle.js

--- Smart_Contracts/TruffleCompilation/truffle.js
+++ Smart_Contracts/TruffleCompilation/truffle.js
@@ -16,7 +16,8 @@ networks: {
 		host: "127.0.0.1",
 		port:8545,
 		network_id: "*",
-		gas: 4612388
+		gas: 4612388,
+		from: '0x08514f2fe0ba20e8347ab2f04dcee9bdca917db6'
 	},
 	toba: {
 		host: "127.0.0.1",
